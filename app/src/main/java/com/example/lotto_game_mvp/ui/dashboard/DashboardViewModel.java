b928b21497e19e3e55fd8c0ccb2e53606d2b3fb4
hanescargot <hanescargot@gmail.com>
2022-04-29 11:46:14 +0900

    WIP

---
+++ app/src/main/java/com/example/lotto_game_mvp/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.lotto_game_mvp.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
